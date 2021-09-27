0b434bd56a600793a1d6834354d19e5cb75f4f0a
Yogasekar <vyogasekar@deloitte.com>
2021-09-27 17:53:48 +0530

    asa

---
+++ taskset/httpcat.py
@@ -0,0 +1,31 @@
+from locust import TaskSet, constant, task, HttpUser, SequentialTaskSet
+
+
+class Httpcat200(SequentialTaskSet ):
+    @task
+    def get_status_200(self):
+        self.client.get("/200")
+        self.interrupt()
+
+
+class Httpcat100300(SequentialTaskSet  ):
+    @task
+    def get_status_100(self):
+        self.client.get("/100")
+        self.interrupt()
+
+    @task
+    def get_status_100(self):
+        self.client.get("/300")
+        self.interrupt()
+
+
+
+
+class Loadtest(HttpUser):
+    host = "http://http.cat"
+    wait_time = constant(1)
+    tasks = [Httpcat200,Httpcat100300]
+
+
+
